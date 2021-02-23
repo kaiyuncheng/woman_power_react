765c25e46c3bd8162d0cf158b8d9be5051ee39ae
kaiyuncheng <kaiyuncheng@adamde-iMac.local>
2021-02-23 18:45:55 +0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+import Header from './components/Header';
+import Hero from './components/Hero';
+
 
 function App() {
   return (
     <div className="App">
+      <Loader
+        className="z-50 fixed bg-primary-light w-screen h-screen flex justify-center items-center"
+        type="Oval"
+        color="#FF398D"
+        height={100}
+        width={100}
+        timeout={5000} //3 secs
+      />
+
+      <Header className="sticky top-0 left-0 z-40 bg-primary-light bg-opacity-50"/>
+      <Hero />
+
+      
+      
       <header className="App-header">
+        <div className="bg-red-500">123
+        <Loader
+        className="bg-black"
+        type="Oval"
+        color="#FF398D"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Hero/index.js
@@ -0,0 +1,42 @@
+import bgHero from "../../images/bg_orange_01.png";
+// import HeroAnimation from './components/Hero/HeroAnimation';
+
+const Hero = () => (
+  <>
+    <div
+      className="section hero w-full -mt-20 pt-20 md:pt-20 lg:pt-20 overflow-hidden bg-right bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${bgHero})` }}
+    >
+      <div className="relative w-full h-full max-w-screen-2xl mx-auto px-3 lg:px-6 flex flex-col justify-center items-center">
+        
+        <div className="btn mb-14">
+          <button
+            type="button"
+            class="animate-bounce scroll_btn relative group text-brown-dark inline-flex items-center hover:text-primary-dark focus:outline-none"
+          >
+            <span class="font-semibold italic mr-4 transform translate-y-0 transition-all duration-500 group-hover:translate-y-2">
+              SCROLL DOWN
+            </span>
+            <svg
+              class="fill-current text-transparent stroke-1 transform transition-all duration-500 translate-y-0 group-hover:translate-y-4 group-hover:stroke-0 group-hover:text-primary-dark"
+              xmlns="http://www.w3.org/2000/svg"
+              width="23.493"
+              height="15.527"
+              viewBox="0 0 23.493 15.527"
+            >
+              <path
+                id="Icon_awesome-angle-down"
+                data-name="Icon awesome-angle-down"
+                stroke="#4C4948"
+                d="M10.055,24.771.492,15.209a1.681,1.681,0,0,1,0-2.384l1.589-1.589a1.681,1.681,0,0,1,2.384,0l6.778,6.778,6.778-6.778a1.681,1.681,0,0,1,2.384,0l1.589,1.589a1.681,1.681,0,0,1,0,2.384l-9.562,9.562a1.672,1.672,0,0,1-2.377,0Z"
+                transform="translate(0.503 -10.24)"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export default Hero;
